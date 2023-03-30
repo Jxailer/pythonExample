67187ae0c0e8ce23a76f0d38591f0308b8fc94f3
Inseo Kang <JacksonXailer@gmail.com>
2023-03-30 16:01:10 +0900

    second commit

--- pythonBigdata_EX01.py
+++ pythonBigdata_EX01.py
@@ -16,21 +16,16 @@ for row in data:
         row[i] = int(row[i].replace(',', ''))
         # 엑셀값에 포함된 ','를 지움
         # deleted ',' in excel chart value
-
-    getOffNum = row[33]+row[35]+row[37]+row[39]
-    # 퇴근 시간에 지하철에서 내린 사람 수의 합
-    # sum of the number of people who get off the subway in rush time
+    for j in range (33, 40, 2):
+        getOffNum += row[j]
+    print(getOffNum)
+    print(row[3])
+    #퇴근 시간에 지하철에서 내린 사람 수의 합
+    #sum of the number of people who get off the subway in rush time
     if getOffNum > mx_station[1]:
         mx_station[0] = row[3]
         mx_station[1] = getOffNum
 
 print("출퇴근 시간에 하차 수가 가장 많은 역은 "+ mx_station[0] +"역 입니다.")
 print("the station with the maximum number of people getting off at 6-10 PM, which is the rush hour is: "+ mx_station[0]+" station")
-
-
-
-
-
-
-
 
